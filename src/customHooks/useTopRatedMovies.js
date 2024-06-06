4291cac8d0e02b9b954647be15b622b2b621c594
swati <swati.srivastav.sde@gmail.com>
2024-06-06 16:58:40 +0530

    reverted changes

--- src/customHooks/useTopRatedMovies.js
+++ src/customHooks/useTopRatedMovies.js
@@ -17,7 +17,7 @@ const useTopRatedMovies = () => {
 
   useEffect(() => {
     !topRatedMovies && getTopRatedMovies();
-  }, [topRatedMovies, dispatch]);
+  }, []);
 };
 
 export default useTopRatedMovies;
