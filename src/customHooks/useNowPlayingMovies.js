13f2d64fe022cadbcc096ee6d3d1c78bc401241f
SWATI SRIVASTAV <swatisr.04gmail.com@Mac.lan>
2026-09-07 12:32:14 +0530

    before dependency security update

--- src/customHooks/useNowPlayingMovies.js
+++ src/customHooks/useNowPlayingMovies.js
@@ -5,13 +5,13 @@ import { useDispatch, useSelector } from "react-redux";
 
 const useNowPlayingMovies = () => {
   const nowPlayingMovies = useSelector(
-    (store) => store.movies?.nowPlayingMovies
+    (store) => store.movies?.nowPlayingMovies,
   );
   const dispatch = useDispatch();
   const getNowPlayingMovies = async () => {
     const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      API_OPTIONS,
     );
     const json = await data.json();
     dispatch(addNowPlayingMovies(json.results));
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/41c789f0-7df5-4219-94c6-c66fe500590a/3149e5eb-4660-4e3d-9e65-b1e615229c64/IN-en-20240513-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
 
 export const LOGO =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158";
+  "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg";
 
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
